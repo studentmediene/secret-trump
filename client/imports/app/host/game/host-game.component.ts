21838bd9b8aa4e4b491fc4ba9ab9b90a8845c8a2
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2016-11-05 18:12:55 +0100

    Checkpoint

--- client/imports/app/host/game/host-game.component.ts
+++ client/imports/app/host/game/host-game.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
+import { Players } from '../../../../../both/collections/players.collection';
 import { Player } from '../../../../../both/models/player.model';
 
 //noinspection TypeScriptCheckImport
@@ -14,20 +16,33 @@ export class HostGameComponent implements OnInit, OnDestroy{
     gameId: string;
     private sub: any;
 
+    players: Player[];
     president: Player;
+    counselor: Player;
+    voting: boolean = false;
 
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit():void {
         this.sub = this.route.params.subscribe((params) => {
             this.gameId = params['id'];
+
+            this.playGame(this.gameId);
         });
 
+    }
 
+    playGame(gameId) {
+        Players.find({ gameId })
+            .subscribe((p: Player[]) => {
+                
+            });
 
     }
+
 
     ngOnDestroy():void {
         this.sub.unsubscribe();
     }
+
 }
