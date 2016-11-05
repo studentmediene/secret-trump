29319fa1b2c11d443ad9c2e30b85d28f26f05b07
Dag Erik <dagerikhl@gmail.com>
2016-11-05 18:35:19 +0100

    Moved scss from main to vote component

--- client/imports/app/vote/vote.component.ts
+++ client/imports/app/vote/vote.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 //noinspection TypeScriptCheckImport
 import template from './vote.component.html';
-
+//noinspection TypeScriptCheckImport
+import style from './vote.component.scss';
 @Component({
     selector: 'st-vote',
-    template
+    template,
+    styles: [ style ]
 })
 
 export class VoteComponent implements OnInit {
