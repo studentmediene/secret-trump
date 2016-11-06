3925410fab51edf35634917604041fca610f9baf
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2016-11-06 20:28:36 +0100

    Last changes Hackathon SM 2K16

--- client/imports/app/host/game/host-game.component.ts
+++ client/imports/app/host/game/host-game.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { Players } from '../../../../../both/collections/players.collection';
 import { Player } from '../../../../../both/models/player.model';
 import { Elections } from '../../../../../both/collections/elections.collection';
-import { Election } from '../../../../../both/models/election.model';
+import { Election } from '../../../../../both/models/election.model';
 
 //noinspection TypeScriptCheckImport
 import template from './host-game.component.html';
@@ -13,48 +13,134 @@ import template from './host-game.component.html';
 @Component({
     selector: 'st-host-game',
     template,
-    styles: [`
+    styles: [ `
         section {
             width: 60em;
         }
-    `]
+    ` ]
 })
-export class HostGameComponent implements OnInit, OnDestroy{
+export class HostGameComponent implements OnInit, OnDestroy {
     gameId: string;
     private sub: any;
 
     currentRound = 1;
     players: Player[];
+    deadPlayers: Player[];
     president: Player;
     headOfCongress: Player;
 
     election: Election;
+    roundState: string;
     voting: boolean = false;
+    voteComplete: boolean = false;
+    approvedVotes: boolean = false;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) {
+    }
 
-    ngOnInit():void {
+    ngOnInit(): void {
         this.sub = this.route.params.subscribe((params) => {
-            this.gameId = params['id'];
+            this.gameId = params[ 'id' ];
 
             this.playGame(this.gameId);
         });
 
     }
 
-
     playGame(gameId) {
-        Elections.find({gameId}).subscribe((elects: Election[]) => {
-                const elect = elects[this.currentRound-1];
-                this.voting = elect.state === 'voting';
-                this.election = elect;
-                this.currentRound++;
+        Elections.find({ gameId, electionId: this.currentRound })
+            .subscribe((elects: Election[]) => {
+                const [ elect ] = elects;
+                this.checkState(elect);
             });
     }
 
 
+    checkState(election) {
+        let playerSub;
+        const { gameId } = this;
+        const { state, votes } = election;
+        const voteCount = Object.keys(votes).length;
+        this.election = election;
+        this.roundState = state;
 
-    ngOnDestroy():void {
+        switch (state) {
+            case 'select_candidate':
+                this.voting = true;
+                playerSub.unsubscribe();
+                break;
+            case 'voting':
+                this.voting = true;
+                this.voteComplete = false;
+                this.approvedVotes = false;
+
+                //noinspection TypeScriptUnresolvedFunction
+                playerSub = Players.find({gameId}, {sort: {username: 1}})
+                    .zone()
+                    .subscribe((players: Player[]) => {
+                        this.players = players;
+                        let updatedFields = {};
+
+
+                        if (voteCount >= players.length) {
+                            this.voteComplete = true;
+
+                            if(this.candidateApproved(votes)){
+                                this.approvedVotes = true;
+
+                                updatedFields = {
+                                    state: 'pick_cards'
+                                };
+                            } else {
+
+                                updatedFields = {
+                                    headOfCongress: this.getNextPlayer(
+                                        players,
+                                        this.election.headOfCongress),
+                                    candidate: null,
+                                    state: 'select_candidate'
+                                };
+
+                                //console.log(updatedFields);
+                            }
+                            //Elections.update({gameId}, {$set: updatedFields});
+                        }
+
+                    });
+
+                break;
+            case 'pick_cards':
+                this.voting = false;
+                playerSub.unsubscribe();
+                break;
+        }
+
+        //this.currentRound++;
+    }
+
+    getNextPlayer(players, current: Player): Player {
+        if (!players){ return current; }
+
+        console.log(players[1]);
+        const currentIndex = players.indexOf(current);
+        if (currentIndex >= players.length-1){
+            return players[0];
+        } else {
+            return players[currentIndex];
+        }
+    }
+
+    candidateApproved(votes): boolean {
+        let yesVoters = 0;
+        for(const vote in votes) {
+            if(votes[vote]){
+                yesVoters += 1;
+            }
+        }
+        return yesVoters > (Object.keys(votes).length/2);
+    }
+
+    ngOnDestroy(): void {
         this.sub.unsubscribe();
     }
 
--- client/imports/app/host/voting/host-vote.component.ts
+++ client/imports/app/host/voting/host-vote.component.ts
@@ -2,19 +2,55 @@ import { Component, Input } from '@angular/core';
 
 //noinspection TypeScriptCheckImport
 import template from './host-vote.component.html';
+//noinspection TypeScriptCheckImport
+import style from './host-vote.component.scss';
 import { Election } from '../../../../../both/models/election.model';
+import { Player } from '../../../../../both/models/player.model';
 
 @Component({
     selector: 'st-host-vote',
-    template
+    template,
+    styles: [ style ]
 })
 export class HostVoteComponent {
+    _players: Player[];
     _election:Election;
+
+    votes: Object[] = [];
+
     @Input()
     set election(elect: Election) {
         this._election = elect;
+
+        this.checkVotes(this._players, elect);
     }
     get election() {
         return this._election;
     }
+
+    @Input()
+    set players(players: Player[]) {
+        this._players = players;
+        this.checkVotes(players, this._election);
+    }
+    get players() {
+        return this._players;
+    }
+
+    checkVotes(players: Player[], election) {
+        if (!players || !election){
+            return;
+        }
+
+        this.votes = [];
+        for (let user in election.votes) {
+
+            this.votes.push({
+                username: user,
+                vote: election.votes[user]
+            });
+        }
+
+    }
+
 }
